5785f3c0288ff864b8e325df510a396ab87eb745
Adam Ormsby <ormsbyadam@gmail.com>
2021-12-18 19:41:47 -0600

    Day 15 - Chiton part 1 refactored for part 2

--- src/main/kotlin/d15_Chiton/Chiton.kt
+++ src/main/kotlin/d15_Chiton/Chiton.kt
@@ -5,9 +5,6 @@ import util.Coord
 import util.Input
 import util.Output
 
-lateinit var chitonNodeMap: MutableList<MutableList<AStarNode>>
-lateinit var mapEdges: Coord
-
 val moveOptions = listOf(
     Coord(x = 0, y = 1),
     Coord(x = 0, y = -1),
@@ -19,32 +16,47 @@ fun main() {
     Output.day(13, "Chiton")
     val startTime = Output.startTime()
 
-    chitonNodeMap = Input.parseTo2dList(filename = "/input/d15_chiton_risk_level.txt")
+    val chitonMapSmall = Input.parseTo2dList<AStarNode>(filename = "/input/d15_chiton_risk_level.txt")
 
-    val startNode = chitonNodeMap.first().first().apply { gValue = 0 }
-    val goalNode = chitonNodeMap.last().last()
-    mapEdges = goalNode.position
+    val lowestPathRisk = findLowestPathRisk(
+        chitonMapSmall,
+        chitonMapSmall.first().first().apply { gValue = 0 },
+        chitonMapSmall.last().last()
+    )
 
-    chitonNodeMap.forEach { line ->
+    Output.part(1, "Lowest Path Total Risk", lowestPathRisk)
+    Output.part(2, "n/a", "n/a")
+    Output.executionTime(startTime)
+}
+
+// A* notes...
+// F = G + H
+// G = movement cost from the start node to the current square (so cost + parent G)
+// H = estimated movement cost from the current node to the goal node (already calculated)
+
+fun findLowestPathRisk(
+    searchMap: MutableList<MutableList<AStarNode>>,
+    start: AStarNode,
+    goal: AStarNode
+): Int {
+    val mapEdges = goal.position
+
+    searchMap.forEach { line ->
         line.forEach { node ->
-            node.hValue = (mapEdges - node.position).combine()
+            node.hValue = (searchMap.last().last().position - node.position).combine()
         }
     }
 
-    val openNodes = mutableListOf(startNode)
+    val openNodes = mutableListOf(start)
     val closedNodes = mutableListOf<AStarNode>()
     var nextNode = openNodes.first()
 
-    // F = G + H
-    // G = movement cost from the start node to the current square (so cost + parent G)
-    // H = estimated movement cost from the current node to the goal node (already calculated)
-
-    while (nextNode != goalNode) {
+    while (nextNode != goal) {
         val curNode = nextNode
         openNodes.remove(curNode)
         closedNodes.add(curNode)
 
-        val validNeighbors = curNode.getNeighbors().filterNot { it in closedNodes }
+        val validNeighbors = curNode.getNeighbors(searchMap, mapEdges).filterNot { it in closedNodes }
 
         validNeighbors.apply {
             forEach { node ->
@@ -70,28 +82,13 @@ fun main() {
         }
     }
 
-    var lowestPathRisk = 0
-    var s = ""
-    // reverse from end node back to start node
-    while (nextNode != startNode) {
-        lowestPathRisk += nextNode.cost
-        s += nextNode.cost.toString()
-        nextNode = chitonNodeMap[nextNode.parent!!]
-    }
-
-//    println("1121365111511323211")
-//    println(s)
-//    println(lowestPathRisk)
-
-    Output.part(1, "Lowest Path Total Risk", lowestPathRisk)
-    Output.part(2, "n/a", "n/a")
-    Output.executionTime(startTime)
+    return nextNode.gValue!!
 }
 
-fun AStarNode.getNeighbors(): List<AStarNode> =
+fun AStarNode.getNeighbors(map: MutableList<MutableList<AStarNode>>, edges: Coord): List<AStarNode> =
     moveOptions.map { it + position }
-        .filter { it.x > -1 && it.x <= mapEdges.x && it.y > -1 && it.y <= mapEdges.y }
-        .map { chitonNodeMap[it] }
+        .filter { it.x > -1 && it.x <= edges.x && it.y > -1 && it.y <= edges.y }
+        .map { map[it] }
 
 operator fun MutableList<MutableList<AStarNode>>.get(c: Coord) = this[c.x][c.y]
 fun Coord.combine() = x + y
